4579660b6a98b0ceee684bd63e8f64599d523a8d
Cerem Cem ASLAN <cem@aktos.io>
2017-07-26 10:44:17 +0300

    updated to v182

--- mjpg-streamer/install-on-linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-sudo apt-get install libjpeg8-dev imagemagick libv4l-dev
-sudo ln -sf /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
-
--- update.sh
+++ update.sh
@@ -6,7 +6,19 @@ echo "Latest download address is: $addr"
 
 #curl $addr | tar -xvf
 tmp_file="latest.zip"
-curl -Ls -o $tmp_file $addr
-unzip $tmp_file -d mjpg-streamer
+src_path="mjpg-streamer"
 
+
+curl -Ls -o $tmp_file $addr
+rm -r $src_path 2> /dev/null
+unzip $tmp_file -d "latest"
+mv ./latest/mjpg-streamer-code-* $src_path
+rm -r ./latest
+
+echo 
+echo "-------------------------------------------"
+echo
 echo "All done. You can remove $tmp_file any time"
+echo 
+echo "-------------------------------------------"
+echo
